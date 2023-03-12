2c51b7580448317697e357f687cf062f117390a0
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 18:44:22 +0100

    change the charcode of the list element
    change the charcode of the list element and return the new string

--- Ceasers Cipher.js
+++ Ceasers Cipher.js
@@ -1,4 +1,13 @@
 //created rot13 function
 function rot13(str) {
     //split input string
-    let strl=str.split('');}
+    let strl=str.split('');
+    //shift each element of the strl list 13 point forward or backward within the aplhabetical range 
+    //create a new list of this elements and join them
+    return strl.reduce(function(encl,a){if(/[a-z0-9]/i.test(a)){
+        if(a.charCodeAt(0)>='A'.charCodeAt(0)+13){ return encl.concat(String.fromCharCode(a.charCodeAt(0)-13)) }
+        else { return encl.concat(String.fromCharCode(a.charCodeAt(0)+13)); }
+    }else {return encl.concat(a); }},[]).join('')}
+      
+    
+      
